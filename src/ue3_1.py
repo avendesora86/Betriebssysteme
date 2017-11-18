6dae16b4676bb8c5581ef61f55de5c2ef94221d7
avendesora86 <shari.donath@gmx.de>
2017-11-18 14:16:35 +0100

    Initial commit

---
+++ src/ue3_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from __future__ import print_function
+import sys
+
+# print 'Number of arguments: ', len(sys.argv), 'arguments.'
+# print 'Argumentlist: ', str(sys.argv)
+# print 'Last two arguments: ', sys.argv[len(sys.argv) - 2], sys.argv[len(sys.argv) - 1], '.'
+
+if len(sys.argv) > 1:
+    del sys.argv[0]
+    print(sys.argv[len(sys.argv) - 2] + " " + sys.argv[len(sys.argv) - 1]) if len(sys.argv) > 1 else print(
+        sys.argv[len(sys.argv) - 1])
+else:
+    print("No arguments...")
